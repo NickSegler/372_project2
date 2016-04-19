269916186ac8e86ad7beb7ec868de2f46f20f81b
Nick Segler <njsegler@alaska.edu>
2016-04-18 21:36:17 -0800

    B_shapes and Mod_shapes work

--- src/shape.cpp
+++ src/shape.cpp
@@ -16,44 +16,159 @@ using std::runtime_error;
 #include <utility>
 using std::pair;
 
+//this uses the formula provided
+pair<double,double> polygonPoint(double l, int n, int k)
+{
+    double x, y;
+    
+    x = ((l/2)*((sin(((2*k+1)*3.14)/n)/sin(3.14/n))));
+    y = ((-l/2)*((cos(((2*k+1)*3.14)/n)/sin(3.14/n))));
+    
+    return make_pair(x,y);
+}
 
-string Rectangle::draw(const pair<double, double> & coord, bool to_file){
-    string output = "% ! \n";
-    output += "0 0\n";
-    output += "newpath \n";
-    output += std::to_string(coord.first - (width_/2));
-    output += " ";
-    output += std::to_string(coord.second - (height_/2));
-    output += " translate \n";
-    output += "moveto \n";
-    output += std::to_string(b_box.bleft.first);
-    output += " ";
-    output += std::to_string(b_box.bleft.second);
-    output += " lineto \n";
-    output += std::to_string(b_box.bright.first);
-    output += " ";
-    output += std::to_string(b_box.bright.second);
-    output += " lineto \n";
-    output += std::to_string(b_box.tright.first);
-    output += " ";
-    output += std::to_string(b_box.tright.second);
-    output += " lineto \n";
-    output += std::to_string(b_box.tleft.first);
-    output += " ";
-    output += std::to_string(b_box.tleft.second);
-    output += " lineto \n";
-    output += "closepath \n";
+string psTo(double x, double y, int t)
+{
+    string rStr;
+    
+    rStr += std::to_string(x);
+    rStr += " ";
+    rStr += std::to_string(y);
+    
+    if(t==0)
+    {
+        rStr += " moveto \n";
+    }else
+    {
+        rStr += " lineto \n ";
+    }
     
     
-    output += "stroke";
+    return rStr;
+}
+
+//uses the inradius to find sidelength
+double Polygon::sideL(double r, int sides)
+{
+    return (2*r*(tan(180/sides)));
+}
+
+//string Rectangle::draw(const pair<double, double> & coord, bool to_file){
+//    string output = "% ! \n";
+//    output += "0 0\n";
+//    output += "newpath \n";
+//    output += std::to_string(coord.first - (width_/2));
+//    output += " ";
+//    output += std::to_string(coord.second - (height_/2));
+//    output += " translate \n";
+//    output += "moveto \n";
+//    output += std::to_string(b_box.bleft.first);
+//    output += " ";
+//    output += std::to_string(b_box.bleft.second);
+//    output += " lineto \n";
+//    output += std::to_string(b_box.bright.first);
+//    output += " ";
+//    output += std::to_string(b_box.bright.second);
+//    output += " lineto \n";
+//    output += std::to_string(b_box.tright.first);
+//    output += " ";
+//    output += std::to_string(b_box.tright.second);
+//    output += " lineto \n";
+//    output += std::to_string(b_box.tleft.first);
+//    output += " ";
+//    output += std::to_string(b_box.tleft.second);
+//    output += " lineto \n";
+//    output += "closepath \n";
+//    
+//    
+//    output += "stroke";
+//    if(!to_file)
+//        return output;
+//    else{
+//        ofstream write("yourprogram.ps");
+//        if(!write)
+//            throw runtime_error("invalid filename");
+//        
+//        write << output;
+//        
+//        write.close();
+//        return "";
+//    }
+//}
+//
+
+void Polygon::makeBBox()
+{
+    if(numberSides_%2==1)
+    {
+        double h,w;
+        w=(sideLength_*((sin((3.14*(numberSides_-1))/2*numberSides_))/sin(3.14/numberSides_)));
+        h=(sideLength_*((1+cos(3.14/numberSides_))/2*sin(3.14/numberSides_)));
+        
+        b_box.bright = make_pair(w, 0);
+        b_box.tright = make_pair(w, h);
+        b_box.tleft  = make_pair(0, h);
+        
+    }else if (numberSides_%2==0 && numberSides_%4!=0)
+    {
+        double h,w;
+        w=(sideLength_*(1/(sin(3.14/numberSides_))));
+        h=(sideLength_*((cos(3.14/numberSides_))/(sin(3.14/numberSides_))));
+        
+        b_box.bright = make_pair(w, 0);
+        b_box.tright = make_pair(w, h);
+        b_box.tleft  = make_pair(0, h);
+    }else
+    {
+        double h,w;
+        w=(sideLength_*((cos(3.14/numberSides_))/(sin(3.14/numberSides_))));
+        h=(sideLength_*((cos(3.14/numberSides_))/(sin(3.14/numberSides_))));
+        
+        b_box.bright = make_pair(w, 0);
+        b_box.tright = make_pair(w, h);
+        b_box.tleft  = make_pair(0, h);
+    }
+}
+
+void Polygon::makeVList()
+{
+    for(int i =0; i<numberSides_; ++i)
+    {
+        verts.push_back(polygonPoint(sideLength_ ,numberSides_ ,i));
+    }
+}
+
+string B_shape::draw(const pair<double, double> & coord, bool to_file)
+{
+    string outStr;
+    
+    outStr = "% ! \n";
+    outStr += "0 0\n";
+    outStr += "newpath \n";
+    outStr += "gsave \n";
+    outStr += std::to_string(coord.first);
+    outStr += " ";
+    outStr += std::to_string(coord.second);
+    outStr += " translate \n";
+
+    
+    for(int i =0; i<verts.size(); ++i)
+    {
+        outStr += psTo(verts[i].first, verts[i].second, i);
+    }
+    
+    outStr += "closepath \n";
+    outStr += "stroke \n";
+    outStr += "grestore \n";
+    
     if(!to_file)
-        return output;
+        return outStr;
     else{
         ofstream write("yourprogram.ps");
         if(!write)
-            throw runtime_error("invalid filename");
+        throw runtime_error("invalid filename");
         
-        write << output;
+        write << outStr;
         
         write.close();
         return "";
--- src/shape.h
+++ src/shape.h
@@ -12,6 +12,11 @@ using std::string;
 #include <utility>
 using std::pair;
 using std::make_pair;
+#include <vector>
+using std::vector;
+#include <cmath>
+
+#include <iostream>
 
 // Shape
 // Virtual class that all others are derived from
@@ -21,7 +26,7 @@ using std::make_pair;
 //      this should be never used for anything
 class Shape{
 public:
-    ~Shape()
+    virtual ~Shape()
     {}
     virtual  string draw(const pair<double, double> & coord, bool to_file) = 0;
 };
@@ -36,8 +41,16 @@ public:
 //      this should be never used for anything
 class B_shape : public Shape{
 public:
-    virtual string draw(const pair<double, double> & coord, bool to_file) = 0;
+    
+    // draw
+    // takes point for center
+    // bool decides if postscript code get written or returned
+    virtual string draw(const pair<double, double> & coord, bool to_file);
+    
+    vector<pair<double,double>> verts;
+    
 protected:
+    
     struct bounding_box{
         pair<double, double> bleft = make_pair(0,0);
         pair<double, double> bright;
@@ -62,12 +75,13 @@ public:
         b_box.bright = make_pair(x, 0);
         b_box.tright = make_pair(x, y);
         b_box.tleft  = make_pair(0, y);
+        
+        verts.push_back(make_pair(0,0));
+        verts.push_back(make_pair(0,y));
+        verts.push_back(make_pair(x,y));
+        verts.push_back(make_pair(x,0));
     }
     
-    // draw
-    // takes point for center
-    // bool decides if postscript code get written or returned
-    virtual string draw(const pair<double, double> & coord, bool to_file);
 protected:
     bounding_box b_box;
     double height_;
@@ -82,11 +96,127 @@ class Spacer : public Rectangle{
     public:
     Spacer(int x, int y):Rectangle(x, y)
     {}
-    string draw(const pair<double, double> & coord, bool to_file);
+    
 protected:
     bounding_box b_box;
     double height_;
     double width_;
+};
+
+class Polygon : public B_shape{
+public:
+    Polygon():sideLength_(10),numberSides_(4)
+    {
+        makeBBox();
+        makeVList();
+    }
+    
+    Polygon(double l, int n):sideLength_(l),numberSides_(n)
+    {
+        makeBBox();
+        makeVList();
+    }
+    
+    Polygon(double w, double h, int n):numberSides_(n)
+    {
+        if(n%2==0)
+        {
+            sideLength_ = sideL(h,numberSides_);
+        }else{
+            sideLength_ = sideL(w,numberSides_);
+        }
+        makeBBox();
+        makeVList();
+    }
+    
+    void scale(double x, double y)
+    {
+        for(int i=0; i<verts.size(); ++i)
+        {
+            verts[i].first = verts[i].first*x;
+            verts[i].second = verts[i].second*y;
+        }
+    }
+    
+    void makeBBox();
+    void makeVList();
+    double sideL(double r, int sides);
+    
+    //DEBUG INFO
+    void printPoints()
+    {
+        for(auto x:verts)
+        {
+            std::cout << x.first <<" " <<x.second <<std::endl;
+        }
+    }
+    
+protected:
+    bounding_box b_box;
+    double sideLength_;
+    int numberSides_;
+};
+
+class Mod_shape : public Shape{
+public:
+    Mod_shape(B_shape * bsp):bShapeP_(bsp)
+    {
+        bShapeP_=bsp;
+    }
+protected:
+    B_shape * bShapeP_;
+};
+
+class S_shape : public Mod_shape{
+public:
+    S_shape(B_shape * bsp, double x, double y):Mod_shape(bsp),scaleX_(x),scaleY_(y)
+    {
+        scale();
+    }
+    
+    void scale()
+    {
+        for(int i=0; i<bShapeP_->verts.size();++i)
+        {
+            bShapeP_->verts[i].first*=scaleX_;
+            bShapeP_->verts[i].second*=scaleY_;
+        }
+    }
+    
+    virtual  string draw(const pair<double, double> & coord, bool to_file)
+    {
+        return bShapeP_->draw(coord, to_file);
+    }
+    
+protected:
+    double scaleX_;
+    double scaleY_;
+    
+};
+
+class R_shape : public Mod_shape{
+public:
+    R_shape(B_shape * bsp, double angle):Mod_shape(bsp), angle_(angle)
+    {}
+    
+    virtual  string draw(const pair<double, double> & coord, bool to_file)
+    {
+        string outStr = bShapeP_->draw(coord, to_file);
+        
+        string add = std::to_string(angle_) + " rotate \n";
+        
+        int rLoc = outStr.find("translate \n") + 11; //12 is the size of the searched string
+        
+        
+        outStr.insert(rLoc, add);
+        
+        return outStr;
+    }
+
+    
+protected:
+    double angle_;
+    
 };
 
 #endif
